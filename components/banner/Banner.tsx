80a328639bc61dfcbe9b8c9a8c71155cd4d154e9
Abideen Olafimihan <olafimihana@gmail.com>
2023-12-12 10:45:30 +0100

    styling for banner added

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -6,11 +6,17 @@ type Props = {
 const Banner = ({ text, handler }: Props) => {
   return (
     <div>
-      <h1>
-        <span>Coffee</span> <span>Connoisseur</span>
+      <h1 className="text-3xl md:text-5xl font-extrabold flex flex-col md:flex-row md:gap-2 mb-7">
+        <span className="text-white">Coffee</span>{" "}
+        <span className="text-[#3A31C0]">Connoisseur</span>
       </h1>
-      <p>Discover your local coffee shops!</p>
-      <button className="border" onClick={handler}>
+      <p className="font-medium text-[15px] text-white">
+        Discover your local coffee shops!
+      </p>
+      <button
+        className="bg-[#463DE1] py-[1rem] px-[2.5rem] text-sm text-white my-4"
+        onClick={handler}
+      >
         {text}
       </button>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </Head>
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        className={`flex min-h-screen flex-col items-center justify-center p-10 md:p-24 ${inter.className}`}
       >
         <Banner text="View stores nearby" handler={findStores} />
       </main>
