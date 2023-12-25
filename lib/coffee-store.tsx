54e0404e50423fbfbfcca96ee16cee99073a7f1f
Abideen Olafimihan <olafimihana@gmail.com>
2023-12-25 11:02:57 +0100

    successfully populated the coffee store with images from unsplash

--- lib/coffee-store.tsx
+++ lib/coffee-store.tsx
@@ -1,3 +1,10 @@
+//To create an API for unsplash images
+import { createApi } from "unsplash-js";
+const unsplash = createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY!,
+});
+
+//function that returns url to get coffee stores
 const getUrlForCoffeeStores = (
   query: string,
   longlat: string,
@@ -6,7 +13,25 @@ const getUrlForCoffeeStores = (
   return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${longlat}&limit=${limit}`;
 };
 
+//function to returns an array of url for unsplash images
+const getCoffeeStoresPhoto = async () => {
+  const photos = await unsplash.search.getPhotos({
+    query: "coffee shop",
+    page: 1,
+    perPage: 30,
+  });
+
+  const unsplashImage = photos.response?.results.map((result) => {
+    return result.urls.small;
+  });
+
+  return unsplashImage;
+};
+
 export const fetchStores = async () => {
+  //this calls the function to returns an array of url for unsplash images
+  const photos = await getCoffeeStoresPhoto();
+
   const options = {
     method: "GET",
     headers: {
@@ -15,10 +40,15 @@ export const fetchStores = async () => {
     },
   };
 
+  //this calls the function that returns url to get coffee stores
   const response = await fetch(
-    getUrlForCoffeeStores("coffee", "43.651229838005%2C-79.38636906500032", 12),
+    getUrlForCoffeeStores("coffee", "43.651229838005%2C-79.38636906500032", 30),
     options
   );
   const data = await response.json();
-  return data.results;
+  const storesData = data.results.map((result: any, index: number) => {
+    return { ...result, imgUrl: photos![index] };
+  });
+  console.log(storesData);
+  return storesData;
 };
